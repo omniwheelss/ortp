782d5dd28acfed0f1a7a74c458fa677a86822e88
Omniwheelss <omniwheelss@gmail.com>
2017-12-05 10:19:15 +0530

    #12 Current Location map address issue corrected

--- current_location.php
+++ current_location.php
@@ -3,40 +3,40 @@
   ?>
 	<?php
 	if(isset($_REQUEST['id'])){
-		$cl_imei = base64_decode($_REQUEST['id']);
-		$Mysql_Query2 = "select *,b.vehicle_no as vehicle_no from device_data a left join device_master b on a.imei =b.imei where a.imei = '".$cl_imei."' order by a.id desc limit 1";
+		$imei = base64_decode($_REQUEST['id']);
+		$Mysql_Query2 = "select *,b.vehicle_no as vehicle_no from device_data a left join device_master b on a.imei =b.imei where a.imei = '".$imei."' order by a.id desc limit 1";
 		$Mysql_Query_Result2 = mysql_query($Mysql_Query2) or die(mysql_error());
 		
 		$device_count2 = mysql_num_rows($Mysql_Query_Result2);
 		if($device_count2 ==1){
 			$device_status = mysql_fetch_array($Mysql_Query_Result2);
-			$gps_move_status[$cl_imei] = $device_status['gps_move_status'];
-			$device_date_stamp[$cl_imei] = $device_status['device_date_stamp'];
-			$mDevice_Date[$cl_imei] = date("d-m-Y",strtotime($device_date_stamp[$cl_imei]));
-			$mDevice_Time[$cl_imei] = date("H:i:s",strtotime($device_date_stamp[$cl_imei]));
-			$ign[$cl_imei] = $device_status['ign'];
-			$latitude[$cl_imei] = $device_status['latitude'];
-			$longitude[$cl_imei] = $device_status['longitude'];
-			$location[$cl_imei] = $device_status['location'];
-			$message[] = $location[$cl_imei];
-			$speed[$cl_imei] = $device_status['speed'];
-			$id[$cl_imei] = $device_status['id'];
-			$vehicle_no[$cl_imei] = $device_status['vehicle_no'];
+			$gps_move_status = $device_status['gps_move_status'];
+			$device_date_stamp = $device_status['device_date_stamp'];
+			$mDevice_Date = date("d-m-Y",strtotime($device_date_stamp));
+			$mDevice_Time = date("H:i:s",strtotime($device_date_stamp));
+			$ign = $device_status['ign'];
+			$latitude = $device_status['latitude'];
+			$longitude = $device_status['longitude'];
+			$location = $device_status['location'];
+			$message[] = $location;
+			$speed = $device_status['speed'];
+			$id = $device_status['id'];
+			$vehicle_no = $device_status['vehicle_no'];
 		}	
 	}
-
+	
 	?>
   <!-- Right side column. Contains the navbar and content of the page -->
             <aside class="right-side">
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        Dashboard
+                        Current Location Of Vehicle - <b><?=$vehicle_no?>
                         <small></small>
                     </h1>
                     <ol class="breadcrumb">
                         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                        <li class="active">Dashboard</li>
+                        <li class="active">Current Location Of Vehicle</li>
                     </ol>
                 </section>
 
@@ -44,7 +44,7 @@
                 <section class="content">
 
 					<?php
-						include_once("box.php");
+						//include_once("box.php");
 					?>					
                     <!-- top row -->
                     <div class="row">
@@ -63,17 +63,17 @@
                         <section class="col-lg-12 connectedSortable">
                             <!-- Map box -->
                             <div class="box box-primary">
-                                <div class="box-header">
-                                    <!-- tools box -->
-                                    <div class="pull-right box-tools">                                        
-                                        <!--<button class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" title="Date range"><i class="fa fa-calendar"></i></button>-->
+                                <!--<div class="box-header">
+                                    <!-- tools box --
+                                  <div class="pull-right box-tools">                                        
+                                        <!--<button class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" title="Date range"><i class="fa fa-calendar"></i></button>--
                                         <button class="btn btn-primary btn-sm pull-right" data-widget='collapse' data-toggle="tooltip" title="Collapse" style="margin-right: 5px;"><i class="fa fa-minus"></i></button>
-                                    </div><!-- /. tools -->
+                                    </div><!-- /. tools --
 
                                     <i class="fa fa-map-marker"></i>
                                     <h3 class="box-title">
-                                       Current Location of Vehicle - <b><?=$vehicle_no[$cl_imei]?></b>
-                                    </h3>
+                                       Current Location of Vehicle - <b><?=$vehicle_no?></b>
+                                    </h3>-->
 									<script type="text/javascript">
 										
 										function initialize() {
@@ -81,7 +81,7 @@
 											var message;
 											
 										  var mapOptions = {
-											center: new google.maps.LatLng(<?=$latitude[$cl_imei]?>,<?=$longitude[$cl_imei]?>),
+											center: new google.maps.LatLng(<?=$latitude?>,<?=$longitude?>),
 											zoom: <?=$currentlocationzoomlvl;?>
 										  };
 
@@ -93,8 +93,8 @@
 											?>
 										  // Add 5 markers to the map at random locations
 											var position = new google.maps.LatLng(
-												<?=$latitude[$cl_imei]?>,
-												<?=$longitude[$cl_imei]?>);
+												<?=$latitude?>,
+												<?=$longitude?>);
 											var marker = new google.maps.Marker({
 											  position: position,
 											  map: map
@@ -104,11 +104,11 @@
 											attachSecretMessage(marker, <?=$i?>);
 										<?php
 												if(count($message) > 0)
-													//$messages = "'<div  style=\"text-align: center;\" ><b>Vehicle</b>: ".$vehicle_no[$cl_imei]."<br><b>Date</b>:".$mDevice_Date[$cl_imei]."  <b>Time</b>:".$mDevice_Time[$cl_imei]." <b>Speed</b>:".$speed[$cl_imei]."<br>@<br>".join("','",$message)."</div>'";
+													//$messages = "'<div  style=\"text-align: center;\" ><b>Vehicle</b>: ".$vehicle_no."<br><b>Date</b>:".$mDevice_Date."  <b>Time</b>:".$mDevice_Time." <b>Speed</b>:".$speed."<br>@<br>".join("','",$message)."</div>'";
 												
-													$Map_DateTime[$imei] = date("d-M-Y g:ia",strtotime($device_date_stamp[$cl_imei]));
+													$Map_DateTime = date("d-M-Y g:ia",strtotime($device_date_stamp));
 													
-													$messages = "'<div><table cellpadding=\"5\" cellspacing=\"5\" border=\"0\"><tr><td align=\"left\" valign=\"top\" width=\"50px\" colspan=\"2\" style=\"color:red;\"><b>Current Location Info</b></td></tr><tr><td align=\"left\" valign=\"top\" width=\"90px\"><b>Vehicle</b></td><td>".$vehicle_no[$cl_imei]."</td></tr><tr><td align=\"left\" valign=\"top\" width=\"90px\"><b>Date & Time</b></td><td align=\"left\" valign=\"top\">".$Map_DateTime[$imei]."</td></tr><tr><td align=\"left\" valign=\"top\"><b>Location</b></td><td align=\"left\" valign=\"top\">".$location[$imei]."</td></tr></table></div>'";
+													$messages = "'<div><table cellpadding=\"5\" cellspacing=\"5\" border=\"0\"><tr><td align=\"left\" valign=\"top\" width=\"50px\" colspan=\"2\" style=\"color:red;\"><b>Current Location Info</b></td></tr><tr><td align=\"left\" valign=\"top\" width=\"90px\"><b>Vehicle</b></td><td>".$vehicle_no."</td></tr><tr><td align=\"left\" valign=\"top\" width=\"90px\"><b>Date & Time</b></td><td align=\"left\" valign=\"top\">".$Map_DateTime."</td></tr><tr><td align=\"left\" valign=\"top\"><b>Location</b></td><td align=\"left\" valign=\"top\">".$location."</td></tr></table></div>'";
 												
 											}
 												
@@ -132,9 +132,9 @@
 										google.maps.event.addDomListener(window, 'load', initialize);
 
 									</script>			
-                                </div>
+                                <!--</div>-->
                                 <div class="box-body no-padding">
-                                    <div id="map" style="height: 300px;"></div>
+                                    <div id="map" style="height: 500px;"></div>
                                 </div><!-- /.box-body-->
                                 <div class="box-footer">
                                 </div>
